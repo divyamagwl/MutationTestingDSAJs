c6a5a483ef7df764f74f29b94a767e33d93228ef
Divyam <agrawaldivyam2001@gmail.com>
2022-11-28 02:04:17 +0530

    Format document

--- Sorting/index.js
+++ Sorting/index.js
@@ -353,28 +353,28 @@ const combSort = (arr) => {
 
 // Pigeonhole Sort
 const pigeonholeSort = (nums) => {
-    if(nums.length == 0) return [];
+    if (nums.length == 0) return [];
     let min_val = nums[0]
     let max_val = nums[0]
-  
+
     for (let i = 0; i < nums.length; i++) {
-      if (nums[i] > max_val) {max_val = nums[i]}
-      if (nums[i] < min_val) {min_val = nums[i]}
+        if (nums[i] > max_val) { max_val = nums[i] }
+        if (nums[i] < min_val) { min_val = nums[i] }
     }
-  
+
     const range = max_val - min_val + 1
     const data = Array(range).fill(0)
-  
+
     for (let i = 0; i < nums.length; i++) {
-      data[nums[i] - min_val]++
+        data[nums[i] - min_val]++
     }
-  
+
     let index = 0
-  
+
     for (let j = 0; j < range; j++) {
-      while (data[j]-- > 0) {
-        nums[index++] = j + min_val
-      }
+        while (data[j]-- > 0) {
+            nums[index++] = j + min_val
+        }
     }
     return nums
 }
