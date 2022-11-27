ea70ec910d7758206b9db2f101578f422de06c73
Divyam <agrawaldivyam2001@gmail.com>
2022-11-28 00:47:36 +0530

    Added Merge sort with tc

--- Sorting/index.js
+++ Sorting/index.js
@@ -120,10 +120,42 @@ const partition = (arr, start, end) => {
     return p;
 }
 
+// Merge Sort
+const Merge = (left, right) => {
+    let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            resultArray.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            resultArray.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return resultArray
+        .concat(left.slice(leftIndex))
+        .concat(right.slice(rightIndex));
+}
+
+const mergeSort = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let middle = Math.floor(arr.length/2);
+    let left = arr.slice(0, middle);
+    let right = arr.slice(middle);
+
+    return Merge(mergeSort(left), mergeSort(right));
+
+}
 module.exports = {
     bubbleSort,
     countSort,
     insertionSort,
     selectionSort,
-    quickSort
+    quickSort,
+    mergeSort
 };
