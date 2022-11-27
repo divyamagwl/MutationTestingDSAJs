dacedd40a76b0863314ad067df0d0801d5e8b70f
Divyam <agrawaldivyam2001@gmail.com>
2022-11-28 02:02:32 +0530

    Added Pigeon hole sort with tc

--- Sorting/index.js
+++ Sorting/index.js
@@ -280,6 +280,7 @@ const oddEvenSort = (arr) => {
     return arr;
 }
 
+// Shell Sort
 const shellSort = (arr) => {
     var increment = arr.length / 2;
     while (increment > 0) {
@@ -304,6 +305,7 @@ const shellSort = (arr) => {
     return arr;
 }
 
+// Comb Sort
 const combSort = (arr) => {
     const is_array_sorted = (arr) => {
         var sorted = true;
@@ -349,6 +351,34 @@ const combSort = (arr) => {
     return arr;
 }
 
+// Pigeonhole Sort
+const pigeonholeSort = (nums) => {
+    if(nums.length == 0) return [];
+    let min_val = nums[0]
+    let max_val = nums[0]
+  
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > max_val) {max_val = nums[i]}
+      if (nums[i] < min_val) {min_val = nums[i]}
+    }
+  
+    const range = max_val - min_val + 1
+    const data = Array(range).fill(0)
+  
+    for (let i = 0; i < nums.length; i++) {
+      data[nums[i] - min_val]++
+    }
+  
+    let index = 0
+  
+    for (let j = 0; j < range; j++) {
+      while (data[j]-- > 0) {
+        nums[index++] = j + min_val
+      }
+    }
+    return nums
+}
+
 
 module.exports = {
     bubbleSort,
@@ -361,5 +391,6 @@ module.exports = {
     bucketSort,
     oddEvenSort,
     shellSort,
-    combSort
+    combSort,
+    pigeonholeSort
 };
