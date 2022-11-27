d9c9b35b7e4bafdd2eb2477751fd0ae17f1c4cc0
Divyam <agrawaldivyam2001@gmail.com>
2022-11-28 00:39:22 +0530

    Added Quick sort with tc

--- Sorting/index.js
+++ Sorting/index.js
@@ -90,9 +90,43 @@ const selectionSort = (arr) => {
     return arr;
 }
 
+// Quick Sort
+const quickSort = (arr) => {
+    quicksort(arr, 0, arr.length - 1);
+    return arr;
+}
+
+const quicksort = (arr, start, end) => {
+    if (start < end) {
+        let p = partition(arr, start, end);
+        quicksort(arr, 0, p - 1);
+        quicksort(arr, p + 1, end);
+
+    }
+}
+
+const partition = (arr, start, end) => {
+    let p = start;
+    let pivot = arr[end];
+    for (let i = start; i < end; i++) {
+    if (arr[i] <= pivot) {
+        let temp = arr[i];
+        arr[i] = arr[p];
+        arr[p] = temp;
+        p++;
+
+    }
+    }
+    let temp = arr[p];
+    arr[p] = arr[end];
+    arr[end] = temp;
+    return p;
+}
+
 module.exports = {
     bubbleSort,
     countSort,
     insertionSort,
-    selectionSort
+    selectionSort,
+    quickSort
 };
