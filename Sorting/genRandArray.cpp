beb2abaf67f6718dd7c21c26f4e88fdb2c87c620
Divyam <agrawaldivyam2001@gmail.com>
2022-11-27 23:22:43 +0530

    Added Sorting Algo - Bubble Sort

---
+++ Sorting/genRandArray.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int sz;
+    cout << "Enter the size of array:: ";
+    cin >> sz;
+
+    freopen("output.txt", "w", stdout);
+
+    vector<int> randArray(sz);
+    for (int i = 0; i < sz; i++)
+        randArray[i] = rand() % 1000; // Generate number between 0 to 999
+
+    cout << "[";
+
+    for (int i = 0; i < sz-1; i++)
+        cout << randArray[i] << ", ";
+
+    cout << randArray[sz-1] << "]" << endl;
+
+    sort(randArray.begin(), randArray.end());
+
+    cout << "[";
+
+    for (int i = 0; i < sz-1; i++)
+        cout << randArray[i] << ", ";
+
+    cout << randArray[sz-1] << "]" << endl;
+
+    return 0;
+}
---
+++ Sorting/index.js
@@ -0,0 +1,19 @@
+// Bubble Sorting
+const bubbleSort = (arr) => {
+    let finalArr = arr;
+
+    for (let i = 0; i <= finalArr.length; i++) {
+        for (let j = 1; j <= finalArr.length; j++) {
+            if (finalArr[j - 1] > finalArr[j]) {
+                temp = finalArr[j - 1];
+                finalArr[j - 1] = finalArr[j];
+                finalArr[j] = temp;
+            }
+        }
+    }
+    return finalArr;
+};
+
+module.exports = {
+    bubbleSort
+};
